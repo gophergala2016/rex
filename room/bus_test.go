103a98edcc80cb48e49944316c7a8e4b73a528b4
Bryan Matsuo <bryan.matsuo@gmail.com>
2016-01-23 00:29:12 -0800

    test message dispatch

--- room/bus_test.go
+++ room/bus_test.go
@@ -10,7 +10,49 @@ func TestBus(t *testing.T) {
 	b.close()
 }
 
-func TestSubscription(t *testing.T) {
+func TestBusMessage(t *testing.T) {
+	done := make(chan struct{})
+	defer func() {
+		timeout := time.After(time.Second)
+		select {
+		case <-timeout:
+			t.Errorf("timeout terminating")
+		case <-done:
+		}
+	}()
+
+	m := make(chan Msg)
+	h := func(msg Msg) {
+		defer close(done)
+		timeout := time.After(time.Second)
+		select {
+		case <-timeout:
+			t.Errorf("timeout delivering message")
+		case m <- msg:
+		}
+	}
+
+	session := "test session"
+	content := String("test message")
+	b := newBus(h)
+	defer b.close()
+	b.Message(session, content)
+
+	timeout := time.After(time.Second)
+	select {
+	case <-timeout:
+		t.Errorf("timeout delivering message")
+	case msg := <-m:
+		if msg.Session() != session {
+			t.Errorf("session: %v", msg.Session())
+		}
+		if msg.Text() != content.Text() {
+			t.Errorf("content: %v", msg.Text())
+		}
+	}
+}
+
+func TestBusSubscription(t *testing.T) {
 	b := newBus(nil)
 
 	content := String("this is a test")
