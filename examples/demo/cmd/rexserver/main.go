72da2526be7992e1a3ea26bffcf09d6a88ee794a
Bryan Matsuo <bryan.matsuo@gmail.com>
2016-01-23 02:25:20 -0800

    fixes

--- examples/demo/cmd/rexserver/main.go
+++ examples/demo/cmd/rexserver/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+
+	"github.com/coreos/etcd/Godeps/_workspace/src/github.com/codegangsta/cli"
+)
 
 func main() {
-	fmt.Println("TODO")
+	app := cli.NewApp()
+	app.Usage = "Start a REx Demo server"
+	app.Action = ServerMain
+	app.Run(os.Args)
+}
+
+// ServerMain performs the main routine for the demo server.
+func ServerMain(*cli.Context) {
+	log.Printf("[INFO] demo server initializing")
+	log.Printf("[INFO] TODO")
 }
