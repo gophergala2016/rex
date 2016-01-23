0f9a9e8815f927a820e5841aadd26bb11664facb
Bryan Matsuo <bryan.matsuo@gmail.com>
2016-01-23 03:12:07 -0800

    define the room.Server type

--- room/server.go
+++ room/server.go
@@ -4,10 +4,117 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"strconv"
 	"time"
 )
+
+// Room represents a single shared enivornment managed by a server.  The
+// service is advertised using mDNS an must conform to the format specified in
+// RFC 6763 Section 7.  The Name may contain any unicode text excluding ASCII
+// control characters but is recommended to not contain '\n' bytes for display
+// purposes.  An mDNS instance identifier will be generated from the given
+// name, the time and the process identifier.
+type Room struct {
+	Name    string
+	Service string
+}
+
+// ServerConfig controls how a server advertises itself to potential clients as
+// well as miscelaneous communication behaviors.
+type ServerConfig struct {
+	Room    *Room
+	Handler func(msg Msg)
+
+	// Addr is an optional address to bind.  If empty, the address of ":0" will
+	// be used.
+	Addr string
+}
+
+// Server is a server used by a TV application to run a game or collaborative
+// procedure.
+type Server struct {
+	config  *ServerConfig
+	bus     *bus
+	handler *httpBus
+	tcp     *net.TCPListener
+	http    *http.Server
+}
+
+// NewServer initializes a new server, but does not start serving clients.
+func NewServer(config *ServerConfig) *Server {
+	if config == nil {
+		panic("nil config")
+	}
+
+	s := &Server{}
+	s.config = config
+	s.init()
+
+	return s
+}
+
+func (s *Server) init() {
+	if s.bus != nil {
+		panic("already initialized")
+	}
+
+	s.bus = newBus(s.config.Handler)
+	s.handler = newHTTPBus(s.bus)
+
+	s.http = &http.Server{
+		Addr:         s.config.Addr, // FIXME not correct
+		Handler:      s.handler,
+		ReadTimeout:  250 * time.Millisecond,
+		WriteTimeout: 0,
+	}
+}
+
+// Run binds to a random port, begins broadcasting service metadata using mDNS,
+// and begins streaming client events and dispatching client messages.
+// Typically, Run never returns a value. If any critical error is encountered
+// it will be returned.
+func (s *Server) Run() error {
+	err := s.listenTCP()
+	if err != nil {
+		return err
+	}
+	return s.http.Serve(s.tcp)
+}
+
+func (s *Server) listenTCP() error {
+	laddr := &net.TCPAddr{}
+	host, _port, err := net.SplitHostPort(s.config.Addr)
+	if err != nil {
+		return err
+	}
+	laddr.Port, err = strconv.Atoi(_port)
+	if err != nil {
+		return fmt.Errorf("invalid address port")
+	}
+	laddr.IP = net.ParseIP(host)
+	if laddr.IP == nil && host != "" {
+		return fmt.Errorf("invalid address host")
+	}
+	s.tcp, err = net.ListenTCP("tcp", laddr)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Addr returns the string address the bus is listening on for HTTP requests.
+func (s *Server) Addr() string {
+	return s.tcp.Addr().String()
+}
+
+// Event broadcasts c to all connected clients, giving it the next unused event
+// index.
+func (s *Server) Event(c Content) {
+	s.bus.Event(c)
+}
 
 func newBusHandler(b *bus) http.Handler {
 	return newHTTPBus(b)
