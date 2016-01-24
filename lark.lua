1aee5e7c27fb13482e7d94a1e7b9c6b423642646
Bryan Matsuo <bryan.matsuo@gmail.com>
2016-01-24 14:42:17 -0800

    special travis-ci test target to avoid missing shared libraries (cgo)

--- lark.lua
+++ lark.lua
@@ -2,6 +2,10 @@ lark.task{'test', function()
     lark.exec{'sh', '-c', 'go test -cover $(glide novendor)'}
 end}
 
+lark.task{'travis', function()
+    lark.exec{'sh', '-c', 'go test -cover ./room'}
+end}
+
 lark.task{'push', function()
     lark.exec{'git', 'push', 'upstream', 'HEAD', ignore=true}
     lark.exec{'git', 'push', 'origin', 'HEAD', ignore=true}
